5fc597b870905f92bd3a327b63c4d17e077784ec
Lucas <lucascosmo54@outlook.com>
2026-06-01 14:44:18 -0400

    Initial Commit

---
+++ Parable/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Parable
+//
+//  Created by Lucas Cosmo on 2026-06-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Parable/ParableApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParableApp.swift
+//  Parable
+//
+//  Created by Lucas Cosmo on 2026-06-01.
+//
+
+import SwiftUI
+
+@main
+struct ParableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
